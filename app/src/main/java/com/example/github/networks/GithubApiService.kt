11372d367bec7004c52bfed598b8cb72cd273d63
alit adiwiguna <alit.hysteric@gmail.com>
2021-10-13 23:45:50 +0800

    setup header

--- app/src/main/java/com/example/github/networks/GithubApiService.kt
+++ app/src/main/java/com/example/github/networks/GithubApiService.kt
@@ -7,6 +7,7 @@ import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.http.GET
+import retrofit2.http.Headers
 import retrofit2.http.Path
 import retrofit2.http.Query
 
@@ -24,9 +25,11 @@ private val retrofit = Retrofit.Builder()
     .build()
 
 interface GithubApiService {
+    @Headers("Accept: application/vnd.github.v3+json")
     @GET("search/repositories")
     suspend fun getRepositories(@Query("q") filter: String?, @Query("sort") sort: String?): ResponseData<Array<RepositoryEntity>>
 
+    @Headers("Accept: application/vnd.github.v3+json")
     @GET("repos/{owner}/{repo}")
     suspend fun getDetail(@Path("owner") owner: String, @Path("repo") repo: String): RepositoryEntity
 }
